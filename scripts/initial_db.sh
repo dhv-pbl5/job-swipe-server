1b5726e9af6cca824a8af0636c006e65806235fd
hieupt <55039604+hieupt810@users.noreply.github.com>
2024-03-05 21:42:00 +0700

    Feat/db (#1)
    * feat: initial database
    
    * feat: scripts for docker and db

---
+++ scripts/initial_db.sh
@@ -0,0 +1,1 @@
+PGPASSWORD=TwwlZL9j10wyziG3 psql -h localhost -p 6002 -U qh47Qsmu19JJRuMq -d job_swipe -f ../db/initial_db.sql
---
+++ scripts/prepare.sh
@@ -0,0 +1,5 @@
+cd ../
+docker-compose up -d
+sleep 1
+cd ./scripts
+sh initial_db.sh
