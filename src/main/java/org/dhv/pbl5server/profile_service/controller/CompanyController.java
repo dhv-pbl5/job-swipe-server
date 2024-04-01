7e15e45f942878c7470da391046bb8b33b428484
Thanh Vinh <ptvinh203@gmail.com>
2024-04-01 17:14:32 +0700

    Feat: Company profile service done!

--- src/main/java/org/dhv/pbl5server/profile_service/controller/CompanyController.java
+++ src/main/java/org/dhv/pbl5server/profile_service/controller/CompanyController.java
@@ -1,13 +1,92 @@
 package org.dhv.pbl5server.profile_service.controller;
 
+import jakarta.annotation.Nullable;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.dhv.pbl5server.authentication_service.annotation.CurrentAccount;
+import org.dhv.pbl5server.authentication_service.annotation.PreAuthorizeCompany;
+import org.dhv.pbl5server.authentication_service.annotation.PreAuthorizeSystemRoleWithoutAdmin;
+import org.dhv.pbl5server.authentication_service.entity.Account;
+import org.dhv.pbl5server.common_service.model.ApiDataResponse;
+import org.dhv.pbl5server.common_service.utils.CommonUtils;
+import org.dhv.pbl5server.common_service.utils.ObjectValidator;
+import org.dhv.pbl5server.profile_service.model.OtherDescription;
+import org.dhv.pbl5server.profile_service.payload.request.CompanyProfileRequest;
 import org.dhv.pbl5server.profile_service.service.CompanyService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/v1/company")
+@RequestMapping("/v1/profile/company")
 @RequiredArgsConstructor
 public class CompanyController {
     private final CompanyService service;
+    private final ObjectValidator validator;
+
+    @PreAuthorizeSystemRoleWithoutAdmin
+    @GetMapping("")
+    public ResponseEntity<ApiDataResponse> getCompanyProfile(
+        @Nullable @RequestParam("company_id") String companyId,
+        @CurrentAccount Account currentAccount
+    ) {
+        if (CommonUtils.isNotEmptyOrNullString(companyId))
+            return ResponseEntity.ok(ApiDataResponse.successWithoutMeta(service.getCompanyProfileById(companyId)));
+        return ResponseEntity.ok(ApiDataResponse.successWithoutMeta(service.getCompanyProfile(currentAccount)));
+    }
+
+
+    @PreAuthorizeCompany
+    @PatchMapping("")
+    public ResponseEntity<ApiDataResponse> updateCompanyProfile(
+        @Valid @RequestBody CompanyProfileRequest request,
+        @CurrentAccount Account currentAccount
+    ) {
+        return ResponseEntity.ok(ApiDataResponse.successWithoutMeta(service.updateCompanyProfile(currentAccount, request)));
+    }
+
+    @PreAuthorizeCompany
+    @PostMapping("/others")
+    public ResponseEntity<ApiDataResponse> insertOtherDescriptions(
+        @RequestBody List<OtherDescription> request,
+        @CurrentAccount Account currentAccount
+    ) {
+        validator.validate(request);
+        return ResponseEntity.ok(ApiDataResponse.successWithoutMeta(service.insertOtherDescriptions(currentAccount, request)));
+    }
+
+    @PreAuthorizeCompany
+    @PatchMapping("/others")
+    public ResponseEntity<ApiDataResponse> updateOtherDescriptions(
+        @RequestBody List<OtherDescription> request,
+        @CurrentAccount Account currentAccount
+    ) {
+        validator.validate(request);
+        return ResponseEntity.ok(ApiDataResponse.successWithoutMeta(service.updateOtherDescriptions(currentAccount, request)));
+    }
+
+    @PreAuthorizeCompany
+    @DeleteMapping("/others")
+    public ResponseEntity<ApiDataResponse> deleteOtherDescriptions(
+        @RequestBody List<String> ids,
+        @CurrentAccount Account currentAccount
+    ) {
+        service.deleteOtherDescriptions(currentAccount, ids);
+        return ResponseEntity.ok(ApiDataResponse.successWithoutMetaAndData());
+    }
+
+    @PreAuthorizeCompany
+    @GetMapping("/others")
+    public ResponseEntity<ApiDataResponse> getOtherDescriptionById(
+        @Nullable @RequestParam("company_id") String companyId,
+        @RequestParam("other_id") String id,
+        @CurrentAccount Account currentAccount
+    ) {
+        return ResponseEntity.ok(ApiDataResponse.successWithoutMeta(service.getOtherDescriptionById(
+            CommonUtils.isEmptyOrNullString(companyId)
+                ? currentAccount.getAccountId().toString()
+                : companyId,
+            id)));
+    }
 }
--- src/main/java/org/dhv/pbl5server/profile_service/service/impl/CompanyServiceImpl.java
+++ src/main/java/org/dhv/pbl5server/profile_service/service/impl/CompanyServiceImpl.java
@@ -1,21 +1,110 @@
 package org.dhv.pbl5server.profile_service.service.impl;
 
 import lombok.RequiredArgsConstructor;
-import org.dhv.pbl5server.common_service.model.ApiDataResponse;
+import org.dhv.pbl5server.authentication_service.entity.Account;
+import org.dhv.pbl5server.common_service.constant.ErrorMessageConstant;
+import org.dhv.pbl5server.common_service.exception.BadRequestException;
+import org.dhv.pbl5server.common_service.exception.NotFoundObjectException;
+import org.dhv.pbl5server.common_service.repository.CrudDbJsonArrayRepository;
+import org.dhv.pbl5server.profile_service.entity.Company;
+import org.dhv.pbl5server.profile_service.mapper.CompanyMapper;
+import org.dhv.pbl5server.profile_service.model.OtherDescription;
+import org.dhv.pbl5server.profile_service.payload.request.CompanyProfileRequest;
+import org.dhv.pbl5server.profile_service.payload.response.CompanyProfileResponse;
 import org.dhv.pbl5server.profile_service.repository.CompanyRepository;
+import org.dhv.pbl5server.profile_service.service.ApplicationPositionService;
 import org.dhv.pbl5server.profile_service.service.CompanyService;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.util.List;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class CompanyServiceImpl implements CompanyService {
     private final CompanyRepository repository;
+    private final CrudDbJsonArrayRepository<OtherDescription, UUID> otherRepository;
+    private final ApplicationPositionService applicationPositionService;
+    private final CompanyMapper mapper;
 
     @Override
-    public Optional<ApiDataResponse> getCompanyById(UUID id) {
-        return Optional.empty();
+    public CompanyProfileResponse getCompanyProfile(Account account) {
+        var company = getAllDataByAccountId(account.getAccountId());
+        return mapper.toCompanyResponse(company);
+    }
+
+    @Override
+    public CompanyProfileResponse getCompanyProfileById(String accountId) {
+        var company = getAllDataByAccountId(UUID.fromString(accountId));
+        return mapper.toCompanyResponse(company);
+    }
+
+    @Override
+    public CompanyProfileResponse updateCompanyProfile(Account account, CompanyProfileRequest request) {
+        var company = repository.findById(account.getAccountId())
+            .orElseThrow(() -> new NotFoundObjectException(ErrorMessageConstant.COMPANY_PROFILE_NOT_FOUND));
+        company = mapper.toCompany(company, request);
+        repository.save(company);
+        return mapper.toCompanyResponse(getAllDataByAccountId(account.getAccountId()));
+    }
+
+    @Override
+    public CompanyProfileResponse insertOtherDescriptions(Account account, List<OtherDescription> request) {
+        // Remove id if exist
+        request.forEach(e -> e.setId(null));
+        var company = repository.findById(account.getAccountId())
+            .orElseThrow(() -> new NotFoundObjectException(ErrorMessageConstant.COMPANY_PROFILE_NOT_FOUND));
+        company.setOthers(otherRepository.saveAll(company.getOthers(), request));
+        repository.save(company);
+        return mapper.toCompanyResponse(getAllDataByAccountId(account.getAccountId()));
+    }
+
+    @Override
+    public CompanyProfileResponse updateOtherDescriptions(Account account, List<OtherDescription> request) {
+        var company = repository.findById(account.getAccountId())
+            .orElseThrow(() -> new NotFoundObjectException(ErrorMessageConstant.COMPANY_PROFILE_NOT_FOUND));
+        for (var req : request) {
+            if (req.getId() == null)
+                throw new BadRequestException(ErrorMessageConstant.OTHER_DESCRIPTION_ID_IS_REQUIRED);
+            if (!otherRepository.existsById(company.getOthers(), req.getId()))
+                throw new NotFoundObjectException(ErrorMessageConstant.OTHER_DESCRIPTION_NOT_FOUND);
+        }
+        company.setOthers(otherRepository.saveAll(company.getOthers(), request));
+        repository.save(company);
+        return mapper.toCompanyResponse(getAllDataByAccountId(account.getAccountId()));
+    }
+
+    @Override
+    public void deleteOtherDescriptions(Account account, List<String> ids) {
+        var company = repository.findById(account.getAccountId())
+            .orElseThrow(() -> new NotFoundObjectException(ErrorMessageConstant.COMPANY_PROFILE_NOT_FOUND));
+        if (checkDeleteIdsRequest(company.getOthers().stream().map(OtherDescription::getId).toList(), ids)) {
+            var result = otherRepository.deleteAllById(company.getOthers(), ids.stream().map(UUID::fromString).toList());
+            company.setOthers(result);
+            repository.save(company);
+        }
+    }
+
+    @Override
+    public OtherDescription getOtherDescriptionById(String companyId, String id) {
+        var company = repository.findById(UUID.fromString(companyId))
+            .orElseThrow(() -> new NotFoundObjectException(ErrorMessageConstant.COMPANY_PROFILE_NOT_FOUND));
+        return otherRepository.findById(company.getOthers(), UUID.fromString(id))
+            .orElseThrow(() -> new NotFoundObjectException(ErrorMessageConstant.OTHER_DESCRIPTION_NOT_FOUND));
+    }
+
+    @Override
+    public Company getAllDataByAccountId(UUID accountId) {
+        var company = repository.findById(accountId).orElseThrow(() -> new NotFoundObjectException(ErrorMessageConstant.COMPANY_PROFILE_NOT_FOUND));
+        company.setAccount(applicationPositionService.getAccountWithAllApplicationPositions(accountId));
+        return company;
+    }
+
+    private boolean checkDeleteIdsRequest(List<UUID> data, List<String> ids) {
+        for (var id : ids) {
+            if (!data.contains(UUID.fromString(id)))
+                throw new BadRequestException(ErrorMessageConstant.DELETE_IDS_REQUEST_HAVE_ONE_NOT_FOUND);
+        }
+        return true;
     }
 }
