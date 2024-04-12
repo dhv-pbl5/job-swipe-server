3bc6bc74767680d92ed12d2589c94bd4ec5f64b2
Thanh Vinh <ptvinh203@gmail.com>
2024-04-12 22:36:56 +0700

    Fix: update pom.xml file and run server with mvn command

---
+++ scripts/run_server.sh
@@ -0,0 +1,8 @@
+# Run database with docker-compose
+cd ../
+docker-compose up -d
+sleep 2
+PGPASSWORD=TwwlZL9j10wyziG3 psql -h localhost -p 6002 -U qh47Qsmu19JJRuMq -d job_swipe -f db/initial_db.sql
+
+# Run server by mvn command (required maven 3.6.1 version)
+mvn spring-boot:run
