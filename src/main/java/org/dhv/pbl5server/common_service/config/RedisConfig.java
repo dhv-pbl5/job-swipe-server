5956b147c29bcb74bd7f6b6b9a530ddffebb4419
Phạm Thành Vinh <ptvinh203@gmail.com>
2024-04-20 13:55:20 +0700

    Fix: redis connection in production profile (#22)

--- src/main/java/org/dhv/pbl5server/common_service/config/RedisConfig.java
+++ src/main/java/org/dhv/pbl5server/common_service/config/RedisConfig.java
@@ -1,10 +1,14 @@
 package org.dhv.pbl5server.common_service.config;
 
+import lombok.RequiredArgsConstructor;
+import org.dhv.pbl5server.common_service.constant.CommonConstant;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -12,15 +16,29 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @EnableCaching
 @Configuration
+@RequiredArgsConstructor
 public class RedisConfig {
+    private final Environment env;
     @Value("${spring.data.redis.host}")
     private String redisHost;
     @Value("${spring.data.redis.port}")
     private int redisPort;
+    @Value("${spring.profiles.active}")
+    private String profile;
 
     @Bean
     public JedisConnectionFactory jedisConnectionFactory() {
-        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(redisHost, redisPort);
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        configuration.setHostName(redisHost);
+        configuration.setPort(redisPort);
+        if (profile.equals(CommonConstant.PROD_PROFILE)) {
+            configuration.setPassword(env.getProperty("spring.data.redis.password"));
+            configuration.setUsername(env.getProperty("spring.data.redis.username"));
+            return new JedisConnectionFactory(
+                configuration,
+                JedisClientConfiguration.builder().useSsl().build()
+            );
+        }
         return new JedisConnectionFactory(configuration);
     }
 
@@ -28,10 +46,13 @@ public class RedisConfig {
     public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(jedisConnectionFactory());
+        template.setExposeConnection(true);
+        template.setEnableTransactionSupport(true);
         template.setKeySerializer(new StringRedisSerializer());
         template.setHashKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.afterPropertiesSet();
         return template;
     }
 }
--- src/main/java/org/dhv/pbl5server/common_service/constant/CommonConstant.java
+++ src/main/java/org/dhv/pbl5server/common_service/constant/CommonConstant.java
@@ -4,6 +4,8 @@ public class CommonConstant {
 
     public static final String SUCCESS = "success";
     public static final String FAILURE = "failure";
+    public static final String PROD_PROFILE = "prod";
+    public static final String DEV_PROFILE = "dev";
     public static final String SYSTEM_ROLE_PREFIX = "01";
     public static final String JWT_TYPE = "Bearer";
     public static final String UUID_REGEX_PATTERN = "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$";
