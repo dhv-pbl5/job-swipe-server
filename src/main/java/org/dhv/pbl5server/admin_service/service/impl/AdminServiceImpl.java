e74d1a589258e6f460ea6468c91b24af7df2aa20
Thanh Vinh <ptvinh203@gmail.com>
2024-05-08 19:03:05 +0700

    chore: initial account

--- src/main/java/org/dhv/pbl5server/admin_service/service/impl/AdminServiceImpl.java
+++ src/main/java/org/dhv/pbl5server/admin_service/service/impl/AdminServiceImpl.java
@@ -3,7 +3,6 @@ package org.dhv.pbl5server.admin_service.service.impl;
 import lombok.RequiredArgsConstructor;
 import org.dhv.pbl5server.admin_service.service.AdminService;
 import org.dhv.pbl5server.authentication_service.entity.Account;
-import org.dhv.pbl5server.authentication_service.mapper.AccountMapper;
 import org.dhv.pbl5server.authentication_service.payload.request.CompanyRegisterRequest;
 import org.dhv.pbl5server.authentication_service.payload.request.UserRegisterRequest;
 import org.dhv.pbl5server.authentication_service.repository.AccountRepository;
@@ -14,6 +13,7 @@ import org.dhv.pbl5server.common_service.exception.BadRequestException;
 import org.dhv.pbl5server.common_service.exception.NotFoundObjectException;
 import org.dhv.pbl5server.common_service.model.ApiDataResponse;
 import org.dhv.pbl5server.common_service.utils.CommonUtils;
+import org.dhv.pbl5server.common_service.utils.LogUtils;
 import org.dhv.pbl5server.constant_service.entity.Constant;
 import org.dhv.pbl5server.constant_service.enums.SystemRoleName;
 import org.dhv.pbl5server.constant_service.payload.ConstantSelectionRequest;
@@ -37,7 +37,6 @@ public class AdminServiceImpl implements AdminService {
     private final CompanyService companyService;
     private final AuthService authService;
     private final ApplicationPositionService applicationPositionService;
-    private final AccountMapper accountMapper;
     private final PasswordEncoder passwordEncoder;
 
     @Override
@@ -74,7 +73,7 @@ public class AdminServiceImpl implements AdminService {
     public void activateAccount(List<String> accountIds) {
         for (var id : accountIds) {
             Account account = repository.findById(UUID.fromString(id))
-                .orElseThrow(() -> new NotFoundObjectException(ErrorMessageConstant.ACCOUNT_NOT_FOUND));
+                    .orElseThrow(() -> new NotFoundObjectException(ErrorMessageConstant.ACCOUNT_NOT_FOUND));
             if (account.getDeletedAt() == null)
                 throw new BadRequestException(ErrorMessageConstant.ACCOUNT_IS_ACTIVE);
             account.setDeletedAt(null);
@@ -88,7 +87,7 @@ public class AdminServiceImpl implements AdminService {
     public void deactivateAccount(List<String> accountIds) {
         for (var id : accountIds) {
             Account account = repository.findById(UUID.fromString(id))
-                .orElseThrow(() -> new NotFoundObjectException(ErrorMessageConstant.ACCOUNT_NOT_FOUND));
+                    .orElseThrow(() -> new NotFoundObjectException(ErrorMessageConstant.ACCOUNT_NOT_FOUND));
             if (account.getDeletedAt() != null)
                 throw new BadRequestException(ErrorMessageConstant.ACCOUNT_IS_NOT_ACTIVE);
             account.setDeletedAt(CommonUtils.getCurrentTimestamp());
@@ -102,32 +101,32 @@ public class AdminServiceImpl implements AdminService {
     public void initialDefaultAccount() {
         // Admin account
         var adminAccount = Account.builder()
-            .email("admin@gmail.com")
-            .password(passwordEncoder.encode("123456Aa"))
-            .address("54 Nguyen Luong Bang, Hoa Khanh Bac, Lien Chieu, Da Nang")
-            .phoneNumber("0348219257")
-            .build();
+                .email("admin@gmail.com")
+                .password(passwordEncoder.encode("123456Aa"))
+                .address("54 Nguyen Luong Bang, Hoa Khanh Bac, Lien Chieu, Da Nang")
+                .phoneNumber("0348219257")
+                .build();
         // User account
         var userRegisterRequest = UserRegisterRequest.builder()
-            .email("user@gmail.com")
-            .password("123456Aa")
-            .address("54 Nguyen Luong Bang, Hoa Khanh Bac, Lien Chieu, Da Nang")
-            .phoneNumber("0348219257")
-            .dateOfBirth(CommonUtils.getCurrentTimestamp())
-            .lastName("Pham Thanh")
-            .firstName("Vinh")
-            .gender(true)
-            .build();
+                .email("user@gmail.com")
+                .password("123456Aa")
+                .address("54 Nguyen Luong Bang, Hoa Khanh Bac, Lien Chieu, Da Nang")
+                .phoneNumber("0348219257")
+                .dateOfBirth(CommonUtils.getCurrentTimestamp())
+                .lastName("Pham Thanh")
+                .firstName("Vinh")
+                .gender(true)
+                .build();
         // Company account
         var companyRegisterRequest = CompanyRegisterRequest.builder()
-            .email("company@gmail.com")
-            .password("123456Aa")
-            .address("54 Nguyen Luong Bang, Hoa Khanh Bac, Lien Chieu, Da Nang")
-            .phoneNumber("0348219257")
-            .companyName("DHV job swipe")
-            .companyUrl("https://github.com/dhv-pbl5")
-            .establishedDate(CommonUtils.getCurrentTimestamp())
-            .build();
+                .email("company@gmail.com")
+                .password("123456Aa")
+                .address("54 Nguyen Luong Bang, Hoa Khanh Bac, Lien Chieu, Da Nang")
+                .phoneNumber("0348219257")
+                .companyName("DHV job swipe")
+                .companyUrl("https://github.com/dhv-pbl5")
+                .establishedDate(CommonUtils.getCurrentTimestamp())
+                .build();
         for (var item : (List<Constant>) constantService.getSystemRoles(null)) {
             var role = AbstractEnum.fromString(SystemRoleName.values(), item.getConstantName());
             if (role == SystemRoleName.ADMIN)
@@ -137,12 +136,26 @@ public class AdminServiceImpl implements AdminService {
             if (role == SystemRoleName.USER)
                 userRegisterRequest.setSystemRole(new ConstantSelectionRequest(item.getConstantId().toString()));
         }
+        // Create admin account
         try {
+            if (repository.existsByEmail(adminAccount.getEmail()))
+                throw new Exception("Admin account already exists");
             repository.save(adminAccount);
+        } catch (Exception e) {
+            LogUtils.error("Initial account", e.toString());
+        }
+        // Create user account
+        try {
             authService.register(userRegisterRequest);
+
+        } catch (Exception e) {
+            LogUtils.error("Initial account", e.toString());
+        }
+        // Create company account
+        try {
             authService.register(companyRegisterRequest);
-        } catch (Exception ex) {
-            ex.printStackTrace();
+        } catch (Exception e) {
+            LogUtils.error("Initial account", e.toString());
         }
     }
 }
