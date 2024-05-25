9057b9ac3588ac235beaa65782b54fcae43d4596
Thanh Vinh <ptvinh203@gmail.com>
2024-05-25 12:03:34 +0700

    update job-swipe-network in docker

--- scripts/server.sh
+++ scripts/server.sh
@@ -2,6 +2,7 @@ cd ../
 docker-compose up -d
 
 # Run server by mvn command (required maven 3.6.1 version and java 17)
+# Required a application-dev.yml file in resources folder
 mvn clean
 mvn package -DskipTests
 mvn spring-boot:run -Dspring-boot.run.profiles=dev
